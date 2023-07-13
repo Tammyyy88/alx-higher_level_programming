413c1950f25d20d75617fb9b6ff0ce424928db6e
Tammyyy88 <josephtamilore13@gmail.com>
2023-07-12 23:37:28 -0700

    added new files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """Reads a text file and prints its contents to stdout"""
+    """
+    Reads a text file and prints its contents to stdout
 
+    Args:
+        filename (str): Name of the text file to be read
+
+    Returns:
+        None
+    """
     with open(filename, encoding='utf-8') as file:
         print(file.read(), end='')
+
+
+if __name__ == '__main__':
+    read_file("my_file_0.txt")
