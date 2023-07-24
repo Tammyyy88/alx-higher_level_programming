41968106205b8e1aaa1045e05384df4c354b8055
Tammyyy88 <josephtamilore13@gmail.com>
2023-07-24 15:41:55 -0700

    added files

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,30 +1,11 @@
 #!/usr/bin/python3
-def say_my_name(first_name, last_name=""):
-  """
-  Prints My name is <first name> <last name>
-
-  Args:
-    first_name: The first name.
-    last_name: The last name. Defaults to "".
-
-  Raises:
-    TypeError: If first_name or last_name is not a string.
-  """
-
-  if not isinstance(first_name, str):
-    raise TypeError("first_name must be a string")
-
-  if not isinstance(last_name, str):
-    last_name = ""
-
-  print(f"My name is {first_name} {last_name}")
-
+from sys import argv
+say_my_name = __import__('3-say_my_name').say_my_name
 
 if __name__ == "__main__":
-  say_my_name("John", "Smith")
-  say_my_name("Walter", "White")
-  say_my_name("Bob")
-  try:
-    say_my_name(12, "White")
-  except Exception as e:
-    print(e)
+    if len(argv) == 3:
+        say_my_name(argv[1], argv[2])
+    elif len(argv) == 2:
+        say_my_name(argv[1])
+    else:
+        print("Usage: ./3-main.py <first name> <last name>")
