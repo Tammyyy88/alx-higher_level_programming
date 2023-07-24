3d5a00b45dc9a6f429b217b2444f8d8917f7e8f8
Tammyyy88 <josephtamilore13@gmail.com>
2023-07-24 15:26:23 -0700

    added files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,13 +12,10 @@ def add_integer(a, b=98):
     Float arguments are typecasted.
     """
 
-    # Check if a is not an integer or a float
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
-    # Check if b is not an integer or a float
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    # Cast a and b to integers and return their sum
     return int(a) + int(b)
