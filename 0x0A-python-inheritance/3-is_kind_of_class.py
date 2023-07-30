2c4bbcefb2805add2e91258f3e65d2d93d432e8d
Tammyyy88 <josephtamilore13@gmail.com>
2023-07-30 14:07:41 -0700

    added files

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Defines a class and inherited class-checking function"""
 
+
 def is_kind_of_class(obj, a_class):
-    """Returns True if object is instance of or if object is instance of class"""
+    """Returns True if object is instance of object or instance of class"""
     if isinstance(obj, a_class):
         return True
     elif issubclass(type(obj), a_class):
