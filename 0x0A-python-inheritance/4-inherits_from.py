2354f86804c9f77359b13875b845ceb3d0da360c
Tammyyy88 <josephtamilore13@gmail.com>
2023-07-30 14:12:07 -0700

    added files

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,5 +2,7 @@
 
 def inherits_from(obj, a_class):
     """Returns True if the object is instance of class that inherited
-from specified class; otherwise False"""
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    from specified class; otherwise False"""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
