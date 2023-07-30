51b78a9229bfb29424ed640f0eaecf9e9d6f7038
Tammyyy88 <josephtamilore13@gmail.com>
2023-07-30 16:45:04 -0700

    added files

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-from sys import argv
-say_my_name = __import__('3-say_my_name').say_my_name
+"""Defines a name-printing function."""
 
-if __name__ == "__main__":
-    if len(argv) == 3:
-        say_my_name(argv[1], argv[2])
-    elif len(argv) == 2:
-        say_my_name(argv[1])
-    else:
-        print("Usage: ./3-main.py <first name> <last name>")
+
+def say_my_name(first_name, last_name=""):
+    """Print a name.
+
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
